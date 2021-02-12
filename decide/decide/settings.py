e42150c556798b5d26c23148cf1f19bdf7daf412
fersolesp <fersolesp@alum.us.es>
2021-02-12 10:02:12 +0000

    Push a Heroku
    Se ha realizado un push para desplegar el proyecto en heroku

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -184,7 +186,6 @@ if os.path.exists("config.jsonnet"):
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
 
-import django_heroku
 django_heroku.settings(locals())
 
 APIS={}
